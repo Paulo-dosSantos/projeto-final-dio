c3be14271d8c87ed4c7300931e0abb13f10b66cb
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-09-26 15:31:35 -0300

    projeto iniciado

---
+++ src/main/java/com/dio/santanderdev2023/SantanderDev2023Application.java
@@ -0,0 +1,13 @@
+package com.dio.santanderdev2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDev2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/santanderdev2023/SantanderDev2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santanderdev2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
